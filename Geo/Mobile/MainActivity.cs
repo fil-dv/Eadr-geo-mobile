41c56026c024598a2ed08861dc552dbf2bb53f10
fil-eadr <d.filonenko@eadr.com.ua>
2016-09-23 14:39:24 +0300

    Created solution
    Created projects, created mainActivity design.

---
+++ Geo/Mobile/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Mobile
+{
+    [Activity(Label = " ", MainLauncher = true, Icon = "@drawable/logo")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button btnSubmit = FindViewById<Button>(Resource.Id.btn_logIn);
+
+            btnSubmit.Click += BtnSubmit_Click;
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, "Не готово...", ToastLength.Long).Show();
+        }
+    }
+}
+
